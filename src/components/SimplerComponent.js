a280f55106aa3aae9d7b650bbb828d7abfdac768
Matthew Robinson <matt_robinson1@yahoo.com>
2020-01-27 20:41:41 +0000

    Automatically backed up by Learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -2,7 +2,10 @@
 import React from "react";
 
 const SimplerComponent = ({ handleClick}) => {
-  return <div onClick={handleClick}>I am just happy</div>
+  return 
+  <div onClick=
+  {handleClick}>I am just happy
+  </div>
 };
 
 export default SimplerComponent; 
